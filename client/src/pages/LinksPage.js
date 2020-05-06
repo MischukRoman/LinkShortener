394736343fd4eeb81966b403559fa54899762b56
Mischuk Roman <mischuk.romann@gmail.com>
2020-05-06 15:37:26 +0300

    Working version of the application | Added a list of all created links, a working redirect to shortened links and click count

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useContext, useState, useCallback, useEffect} from 'react';
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContext";
+import {Loader} from "../components/Loader";
+import {LinksList} from "../components/LinksList";
 
 export const LinksPage = () => {
+    const [links, setLinks] = useState([]);
+    const {request, loading} = useHttp();
+    const {token} = useContext(AuthContext);
+
+    const fetchLinks = useCallback(async () => {
+        try {
+            const fetched = await request('/api/link','GET', null, {
+                Authorization: `Bearer ${token}`
+            });
+            setLinks(fetched)
+        } catch (e) {
+        }
+    }, [request, token]);
+
+    useEffect(() => {
+        fetchLinks()
+    }, [fetchLinks]);
+
+    if (loading) {
+        return <Loader/>
+    }
+
     return (
-        <div className="container">
-            <h1>Links Page</h1>
-        </div>
+        <>
+            {!loading && <div className="container">
+               <LinksList links={links}/>
+            </div>}
+        </>
     );
 };
